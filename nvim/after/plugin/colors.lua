9ccd029b7ca9b01038e78f1d2274de38445bf866
David Chiang <65947736+daveanthonyc@users.noreply.github.com>
2024-01-04 02:34:44 +1100

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,18 @@
-function ColorMyPencils(color)
-	color = "kanagawa"
+function ColorMyPencils(color) 
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
-
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 end
 
 ColorMyPencils()
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 130 }
+  end,
+})
