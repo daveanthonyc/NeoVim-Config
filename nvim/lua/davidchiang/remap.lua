ec4a722c2bec0d6d1b22ebe9dba7cc3cb34e70ed
David Chiang <65947736+daveanthonyc@users.noreply.github.com>
2024-01-04 02:37:03 +1100

    Update remap.lua

--- nvim/lua/davidchiang/remap.lua
+++ nvim/lua/davidchiang/remap.lua
@@ -1,5 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("i", '<C-s>', '<Esc>:w<CR>')
-vim.keymap.set("n", '<C-s>', ':w<CR>')
+vim.keymap.set('n', '<C-q>', ":!tmux split-window -vhP <CR><C-w>w<CR><C-w>w", { noremap = true, silent = true })
+-- Remap C-d to execute Cd and then zz
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+
+-- remap copy paste from clipboard
+vim.keymap.set('n', '<Leader>y', '"+y', { noremap = true, silent = true })
