b8a9d381b7e9e1ae3ec51d4d81258ff9723ba1cc
David Chiang <65947736+daveanthonyc@users.noreply.github.com>
2024-01-04 02:37:34 +1100

    Update set.lua

--- nvim/lua/davidchiang/set.lua
+++ nvim/lua/davidchiang/set.lua
@@ -18,3 +18,20 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
+-- Set a thick cursor in insert mode
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:bloick,r-cr-o:hor20"
+
+
+vim.cmd([[
+  augroup TransparentCursorLine
+    autocmd!
+    autocmd InsertEnter * highlight CursorLine ctermbg=none guibg=none
+    autocmd InsertLeave * highlight CursorLine guibg=#2f2f2f   
+  augroup END
+]])
